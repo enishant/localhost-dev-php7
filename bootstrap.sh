e1255636a0994ba30b19f4670edb6b9bdb16f13b
Nishant Vaity <enishant@gmail.com>
2019-05-15 13:37:08 +0530

    Install php-xdebug, zip, unzip

--- bootstrap.sh
+++ bootstrap.sh
@@ -27,6 +27,8 @@ sudo apt-get install -y --force-yes apache2 mysql-server git-core nodejs rabbitm
 sudo apt-get install -y --force-yes php7.1-common php7.1-dev php7.1-json php7.1-opcache php7.1-cli libapache2-mod-php7.1
 sudo apt-get install -y --force-yes php7.1 php7.1-mysql php7.1-fpm php7.1-curl php7.1-gd php7.1-mcrypt php7.1-mbstring
 sudo apt-get install -y --force-yes php7.1-bcmath php7.1-zip
+sudo apt-get install -y --force-yes php-xdebug
+sudo apt-get install -y --force-yes zip unzip
 Update
 
 echo "-- Configure PHP &Apache --"
